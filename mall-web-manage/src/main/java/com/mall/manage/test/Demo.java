1d76a7f6b9238192aa430337d5aa427c3b1f3056
hehehe7 <53427026+hehehe7@users.noreply.github.com>
2021-06-09 11:28:44 +0800

    Update Demo.java

--- mall-web-manage/src/main/java/com/mall/manage/test/Demo.java
+++ mall-web-manage/src/main/java/com/mall/manage/test/Demo.java
@@ -13,6 +13,7 @@ public class Demo {
         System.out.println((c-d) == 0.1);
         System.out.println(1.0 / 0);
         System.out.println(0.0 / 0.0);
+        
     }
 
     static <String, T, Alibaba> String get(String string, T t) { return string; }
